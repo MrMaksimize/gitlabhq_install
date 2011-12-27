564a60e4c5eec3d33051e33e652c8ccefcb0476f
Maksim Pecherskiy <max@maksimize.com>
2011-12-27 09:47:39 -0600

    added config file

--- ubuntu_gitolite.sh
+++ ubuntu_gitolite.sh
@@ -8,23 +8,16 @@ sudo adduser \
   --disabled-password \
   --home /home/git \
   git
-
 # Add your user to git group
 sudo usermod -a -G git `eval whoami` 
-
 # copy your pub key to git home
 sudo cp ~/.ssh/id_rsa.pub /home/git/rails.pub
-
 # clone gitolite
 sudo -u git -H git clone git://github.com/gitlabhq/gitolite /home/git/gitolite
-
 # install gitolite
 sudo -u git -H /home/git/gitolite/src/gl-system-install
-
 # Setup (Dont forget to set umask as 0007!!)
 sudo -u git -H sh -c "PATH=/home/git/bin:$PATH; gl-setup ~/rails.pub"
-
 sudo chmod -R g+rwX /home/git/repositories/
 sudo chown -R git:git /home/git/repositories/
-
 echo "Done"
