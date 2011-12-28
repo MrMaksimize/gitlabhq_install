db6ffa40dd5e77895fc340123d685b792eea66e2
Maksim Pecherskiy <max@maksimize.com>
2011-12-27 18:44:59 -0600

    testing fix branch

--- ubuntu_gitlab.sh
+++ ubuntu_gitlab.sh
@@ -1,7 +1,7 @@
 sudo apt-get install python-dev python-pip
 sudo pip install pygments
 sudo gem install bundler
-git clone git://github.com/gitlabhq/gitlabhq.git
+git clone -b fix git://github.com/gitlabhq/gitlabhq.git
 cd gitlabhq
 bundle install --without development test
 bundle exec rake db:setup RAILS_ENV=production
