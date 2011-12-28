9520c19dbe7318913fd6670bdd4fb7b45d92e512
Maksim Pecherskiy <max@maksimize.com>
2011-12-27 19:09:59 -0600

    changed path

--- ubuntu_gitlab.sh
+++ ubuntu_gitlab.sh
@@ -1,7 +1,7 @@
 sudo apt-get install python-dev python-pip
 sudo pip install pygments
 sudo gem install bundler
-git clone -b fix git://github.com/gitlabhq/gitlabhq.git
+git clone -b fix git://github.com/MrMaksimize/gitlabhq.git
 cd gitlabhq
 bundle install --without development test
 bundle exec rake db:setup RAILS_ENV=production
